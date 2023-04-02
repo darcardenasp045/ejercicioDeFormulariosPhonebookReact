ed0425032c4e027de57da4426230917244dc674b
Daniel Cardenas <97079553+darcardenasp045@users.noreply.github.com>
2023-04-02 15:43:10 -0500

    se añade la opcion de hacer un filtro, se comprueba si existe el nombre en la lista anterior, y si ya existe se muestra un aviso.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { useState } from "react";
 import Note from "./Note.js";
 
-const names = [
-  {
-    id: 1,
-    name: "Daniel",
-    phone: 123456
-  },
-  {
-    id: 2,
-    name: "Juan",
-    phone: 453356
-  }
-];
-
-const App = () => {
-  const [persons, setPersons] = useState(names);
+const App = ({ personsNames }) => {
+  const [persons, setPersons] = useState(personsNames);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
+  const [filterValue, setFilterValue] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    const nameExists = persons.some(
+      (person) => person.name.toLowerCase() === newName.toLowerCase()
+    );
+
+    if (nameExists) {
+      alert(`${newName} already exists in the phonebook.`);
+      return;
+    }
 
     const noteToAddToState = {
       id: persons.length + 1,
@@ -28,14 +25,26 @@ const App = () => {
       phone: newPhone
     };
 
-    setPersons(names.concat(noteToAddToState));
+    setPersons(persons.concat(noteToAddToState));
     setNewName("");
     setNewPhone("");
     console.log(noteToAddToState);
   };
 
+  const handleFilterChange = (event) => {
+    setFilterValue(event.target.value);
+  };
+
+  const filteredPersons = filterValue
+    ? persons.filter((person) =>
+        person.name.toLowerCase().includes(filterValue.toLowerCase())
+      )
+    : persons;
+
   return (
     <div>
+      Ingresa la letra que quieres buscar:
+      <input value={filterValue} onChange={handleFilterChange} />
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
@@ -55,10 +64,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((note) => {
+      {filteredPersons.map((note) => {
         return <Note key={note.id} {...note} />;
       })}
-      ...
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -5,9 +5,21 @@ import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+const names = [
+  {
+    id: 1,
+    name: "Daniel",
+    phone: 123456
+  },
+  {
+    id: 2,
+    name: "Juan",
+    phone: 453356
+  }
+];
 
 root.render(
   <StrictMode>
-    <App />
+    <App personsNames={names} />
   </StrictMode>
 );
